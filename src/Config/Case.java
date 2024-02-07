1b47ec81cd5090d9474aef5e1a69ba5dee03413f
Salim <salim.ismaili92@gmail.com>
2024-02-07 20:26:04 +0100

    Rajout de l'attribut Nom pour la Ville et modification du NbDePlace int en boolean Rempli

--- src/Config/Case.java
+++ src/Config/Case.java
@@ -1,12 +1,22 @@
-public record Case(Case CelluleSuperposable, boolean ville, boolean gare, int nbDePlace, Content initialContent) {
+package config;
+
+//3 cas possibles :
+//1) Case Rail : false ou true, false, "", false, false ou true, Choisissez une couleur
+//2) Case Ville ou Gare : false, true, nom_de_la_ville, false ou true, false, NOTHING
+//3) Case Paysage : false, false, "", false, false, NOTHING
+public record Case(Case CelluleSuperposable, boolean ville, String nomVille, boolean gare, boolean rempli, Content initialContent) {
     public enum Content {NOTHING, BLEU, VIOLET, MARRON, NOIR, VERT, JAUNE, NOIRE, ROUGE}
     
+    public String getNomVille(String) {
+    	return nomVille;
+    }
+    
     public boolean unPaysage() {
-    	return initialContent == Content.NOTHING;
+    	return !ville && initialContent == Content.NOTHING;
     }
     
     public boolean unChemin() {
-    	return !ville && nbDePlace > 0;
+    	return !ville && aEncoreDeLaPlace();
     }
     
     public boolean uneVille() {
@@ -22,7 +32,7 @@ public record Case(Case CelluleSuperposable, boolean ville, boolean gare, int nb
     }
     
     public boolean aEncoreDeLaPlace() {
-        return nbDePlace != 0;
+        return rempli;
     }
     
 }
