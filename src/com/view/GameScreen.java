4ee894de1861ad3a06ad0b2d1ef58440d42b0ac7
Mina <xo140597@gmail.com>
2024-03-01 17:49:28 +0100

    petit changement

--- src/com/view/GameScreen.java
+++ src/com/view/GameScreen.java
@@ -14,13 +14,13 @@ public class GameScreen extends JPanel {
     private static int tile_width , tile_height ;
     private int tile_offset_width , tile_offset_height ;
 
-    public GameScreen ( GameFrame frame ){
+    public GameScreen ( GameFrame frame  , int width , int height){
         this.frame = frame ;
-        this.mapScreen = new MapScreen() ;
+        setSize(width , height );
         tile_height = getHeight() / 24 ;
         tile_width = getWidth() / 24 ;
+        this.mapScreen = new MapScreen( tile_width , tile_height ) ;
 
-        setSize( tile_width , tile_height ) ;
         setLayout(new BorderLayout());
         add( mapScreen , BorderLayout.CENTER ) ;
 
--- src/com/view/graphics/VilleGraphics.java
+++ src/com/view/graphics/VilleGraphics.java
@@ -19,7 +19,6 @@ public class VilleGraphics {
 
 
     public VilleGraphics(){
-        super();
     }
 
     /**
