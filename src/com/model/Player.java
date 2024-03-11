082697317aaf822d4b3a08c48d57d6753a8c67ec
Mina <xo140597@gmail.com>
2024-03-11 17:48:28 +0100

    la classe TrainGraphics et normalement finit ( je teste plus tard dans la soirée ( mon pc a plus de batterie ) )

--- src/com/model/Player.java
+++ src/com/model/Player.java
@@ -13,12 +13,70 @@ import java.util.ArrayList;
 public class Player {
 	private String name;
     private int score;
+
+	private final String playerCouleur ;
 	private int niveau; //Si niveau = 0, alors c'est un joueur, si niveau = 1 = bot facile, si niveau = 2 bot moyen, si niveau = 3 bot difficile
     private ArrayList<CarteDestination> destinationsList = new ArrayList<>();//La liste de carte mission du jouer
     private ArrayList<CarteWagon.Couleur> trainList = new ArrayList<>(); //La liste de carte wagon du joueur
 
+	public Player ( String playerCouleur ){
+		this.playerCouleur = playerCouleur ;
+		this.score = 0 ;
+	}
+	private int carteDuJoueur(Rail.Content color){
+		int count = 0;
 
+		for(int i = 0; i < this.trainList.size(); i++) {
+			if(compatibleColor(color,this.trainList.get(i)))count++;
 
+		}
+
+		return count;
+	}
+
+	private void retirerLesCartes(Couleur color, int longeur) {
+		int i = 0, count = longeur;
+
+		while(count != 0) {
+			if(this.trainList.get(i) == color) {
+				this.trainList.remove(i);
+				count--;
+			}else {
+				i++;
+			}
+		}
+	}
+
+	public boolean mettreRoute(Route r){
+		if (r.getLongueur() <= this.carteDuJoueur(r.getCouleur()) && r.getProprietaire() == null){
+			this.retirerLesCartes(r.traducteurCouleur(), r.getLongueur());
+			r = new Route(r.getVille1(), r.getVille2(), r.getLongueur(), r.getCouleur());
+			r.setProprietaire(this);
+			return true;
+		}
+		return false;
+	}
+
+	//ATTENTION ! Si c'est true, passer le prochain tour du joueur.
+	public boolean changerGareEnVille(int x, int y, Plateau p){
+		if(p.positionValide(x, y)){
+			if(p.estUneCaseVille(x, y) && !p.estUneCaseGare(x, y)) {
+				((Ville) p.getPlateau()[x][y]).setGare(true);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean compatibleColor(Content content, Couleur couleur){
+
+		if(couleur == Couleur.LOC){
+			return true;
+		}
+
+		return content.ordinal() == couleur.ordinal();
+
+	}
     public ArrayList<CarteDestination> getDestinationsList() {
         return destinationsList;
     }
@@ -58,66 +116,8 @@ public class Player {
     public void setTrainCard(ArrayList<CarteWagon.Couleur> trainList) {
         this.trainList = trainList;
     }
-    
-    private int carteDuJoueur(Rail.Content color){
-    	int count = 0;
 
-    	for(int i = 0; i < this.trainList.size(); i++) {
-    		if(compatibleColor(color,this.trainList.get(i)))count++;
-
-    	}
-    	
-    	return count;
-    }
-
-    private void retirerLesCartes(Couleur color, int longeur) {
-    	int i = 0, count = longeur;
-    	
-    	while(count != 0) {    		
-    		if(this.trainList.get(i) == color) {
-    			this.trainList.remove(i);
-    			count--;
-    		}else {
-    			i++;
-    		}
-    	}   	
-    }
-
-    public boolean mettreRoute(Route r){
-		if (r.getLongueur() <= this.carteDuJoueur(r.getCouleur()) && r.getProprietaire() == null){
-    		this.retirerLesCartes(r.traducteurCouleur(), r.getLongueur());
-    		r = new Route(r.getVille1(), r.getVille2(), r.getLongueur(), r.getCouleur());
-			r.setProprietaire(this);
-    		return true;
-    	}
-		return false;
-    }
-    
-    //ATTENTION ! Si c'est true, passer le prochain tour du joueur.
-    public boolean changerGareEnVille(int x, int y, Plateau p){
-    	if(p.positionValide(x, y)){
-    		if(p.estUneCaseVille(x, y) && !p.estUneCaseGare(x, y)) {
-    			((Ville) p.getPlateau()[x][y]).setGare(true);
-    			return true;
-    		}
-    	}
-		return false;
-    }
-
-	public boolean compatibleColor(Content content, Couleur couleur){
-
-		if(couleur == Couleur.LOC){
-			return true;
-		}
-
-		return content.ordinal() == couleur.ordinal();
-
+	public String getPlayerCouleur() {
+		return playerCouleur;
 	}
-
-
-
-
-
-
-
 }
--- src/com/view/graphics/RailGraphics.java
+++ src/com/view/graphics/RailGraphics.java
@@ -157,9 +157,12 @@ public class RailGraphics {
      * @param g Graphics
      * @param rail Rail
      */
-    public static void paint(Graphics2D g, Rail rail){
-        BufferedImage image = getImage(rail) ;
-        g.drawImage( image , rail.getX() * width , rail.getY() * height , width , height , null ) ;
+    public static void paint(Graphics2D g, Rail rail) {
+        BufferedImage image = getImage(rail);
+        g.drawImage(image, rail.getX() * width, rail.getY() * height, width, height, null);
+        if (rail.getOccuper()) {
+            TrainGraphics.paint(g, rail);
+        }
     }
 
     /*
