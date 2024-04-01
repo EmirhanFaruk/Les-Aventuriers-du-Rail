2362f724ac28dbf2f9b51b9ca31b86a65e40296a
EmirhanFaruk <emirhansagir74@gmail.com>
2024-04-01 13:16:40 +0200

    nombrePointDistance fini

--- src/com/model/config/carte/CarteManager.java
+++ src/com/model/config/carte/CarteManager.java
@@ -1,12 +1,14 @@
 package com.model.config.carte;
 
 import com.model.Game;
+import com.model.ai.Node;
 import com.model.config.Plateau;
 import com.model.config.Route;
 import com.model.config.Ville;
 import com.model.config.carte.CarteWagon.Couleur;
 
 import java.util.Random;
+import java.util.ArrayList;
 
 import static com.model.config.carte.CarteWagon.Couleur.*;
 
@@ -144,9 +146,49 @@ public class CarteManager {
     }
 
 
+    private int cheminLongueur(ArrayList<Ville> chemin)
+    {
+        int res = 0;
+        while(chemin.size() > 1)
+        {
+            Ville current = chemin.get(0);
+            Ville next = chemin.get(1);
+            res += findLink(current, next).getLongueur();
+            chemin.remove(0);
+        }
+
+        return res;
+    }
+
+    private Route findLink(Ville v1, Ville v2)
+    {
+        ArrayList<Route> routes = v1.getRoutes();
+        int i = 0;
+        while (i < routes.size())
+        {
+            if (links(v1, v2, routes.get(i)))
+            {
+                return v1.getRoutes().get(i);
+            }
+            i++;
+        }
+        return routes.get(0);
+    }
+
+    private boolean links(Ville v1, Ville v2, Route route)
+    {
+        boolean possibility1 = route.getVille1() == v1 && route.getVille2() == v2;
+        boolean possibility2 = route.getVille1() == v2 && route.getVille2() == v1;
+        return possibility1 || possibility2;
+    }
+
+
     public int nombrePointDistance(Ville v1, Ville v2){
-        //TODO
-        return 2;
+        ArrayList<Ville> chemin = Node.findClosestPath(v1, v2, null);
+
+        int longueur = cheminLongueur(chemin);
+
+        return longueur;
     }
 
 
