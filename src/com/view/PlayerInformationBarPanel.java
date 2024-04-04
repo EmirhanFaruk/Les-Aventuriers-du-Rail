668686fa38d4d6d5faa178a9c50a40901795dabb
Mina <xo140597@gmail.com>
2024-04-04 15:33:24 +0200

    java doc

--- src/com/view/PlayerInformationBarPanel.java
+++ src/com/view/PlayerInformationBarPanel.java
@@ -22,6 +22,11 @@ public class PlayerInformationBarPanel extends JPanel {
         afficheInfoAll();
     }
 
+    /**
+     * Donne la couleur du joueur
+     * @param str couleur du joueur
+     * @return Color
+     */
     private Color playerColor(String str) {
         switch (str) {
             case "JAUNE":
@@ -37,6 +42,13 @@ public class PlayerInformationBarPanel extends JPanel {
         }
     }
 
+    /**
+     * Une focntion qui crée un panel pour mettre les informations nécessaires
+     * @param player le joueur
+     * @param bgColor sa couleur
+     * @param font la taille du texte
+     * @return Panel avec toutes les informations
+     */
     private JPanel createPlayerPanel(Player player, Color bgColor, Font font) {
         JPanel panel = new JPanel(new GridLayout(1 , 4 ));
         panel.setBackground(bgColor);
@@ -62,6 +74,10 @@ public class PlayerInformationBarPanel extends JPanel {
         return panel;
     }
 
+
+    /**
+     * Une fonction qui affiche tout les informations des joueurs
+     */
     public void afficheInfoAll(){
         JPanel panel = new JPanel( ) ;
         panel.setLayout( new GridLayout( 1 , 4 ) ) ;
@@ -71,6 +87,10 @@ public class PlayerInformationBarPanel extends JPanel {
         add( panel , "AllInformation") ;
     }
 
+    /**
+     * Une fonction renvoie un panel du joueur courant
+     * @return Jpanel du joueur
+     */
     public JPanel afficheInformationJoueurCourant() {
         Color playerColor = playerColor(playerCourant.getPlayerCouleur());
         assert playerColor != null;
@@ -80,6 +100,10 @@ public class PlayerInformationBarPanel extends JPanel {
         return createPlayerPanel(playerCourant, newColor , playerNameFont);
     }
 
+    /**
+     * Une fonciton qui ajoute au panel les informations des autres joueurs
+     * @param panel le panel à utiliser
+     */
     public void afficheInformationOtherPlayer( JPanel panel ) {
         for (Player otherPlayer : players) {
             if (!otherPlayer.equals(playerCourant)) {
@@ -94,6 +118,10 @@ public class PlayerInformationBarPanel extends JPanel {
         repaint();
     }
 
+    /**
+     * Une fonction qui met a jour la barre d'information avec le nouveau joueur courant
+     * @param newPlayerCourant le nouveau joueur
+     */
     public void updateCurrentPlayer(Player newPlayerCourant) {
         removeAll();
 
@@ -107,6 +135,11 @@ public class PlayerInformationBarPanel extends JPanel {
         repaint();
     }
 
+
+    /**
+     * Une fonction qui change le joueur courant et met à jour
+     * @param playerCourant le nouveau joueur courant
+     */
     public void setPlayerCourant(Player playerCourant ) {
         this.playerCourant = playerCourant ;
         updateCurrentPlayer( playerCourant ) ;
