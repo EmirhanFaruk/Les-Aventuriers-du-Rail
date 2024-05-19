59a147785dc789a93248d364fd28802aec84b701
Salim <salim.ismaili92@gmail.com>
2024-05-19 18:22:37 +0200

    Animation Nuke & Correction/Javadoc

--- src/com/model/Player.java
+++ src/com/model/Player.java
@@ -75,77 +75,84 @@ public class Player {
 		return 0 ;
 	}
 	
+	/**
+	 * Pioche une carte de destination pour le joueur.
+	 *
+	 * @param cm  Le gestionnaire de cartes (CarteManager).
+	 * @param i   L'index de la carte de destination à piocher.
+	 * @return    true si la pioche a réussi, sinon false.
+	 */
 	public boolean piocheCarteDestination(CarteManager cm, int i) {
-		//vérifie s'il ne possède pas déjà la carte destination
-		if(cm.getDestinationsCards()[i] != null) {
-			//donne la carte destination et mets à null pour remplacer
-			this.destinationsList.add(cm.getDestinationsCards()[i]);
-			cm.getDestinationsCards()[i] = null;
-			if( !this.destinationsList.isEmpty() )this.canPlay = true;
-			if(game.getGameFrame().getSound().getclick()) game.getGameFrame().getSound().playSound( "INGAME" ,"carte-dest.wav" );
-			return true;
-		}else {
-			if (game.getGameFrame().getSound().getclick() ) game.getGameFrame().getSound().playSound("INGAME" , "popUp.wav");
-			JOptionPane.showMessageDialog(new JFrame(),"Vous avez déjà pioché cette carte !","Instructions",JOptionPane.WARNING_MESSAGE);
-		}
-		
-		return false;
+	    // vérifie s'il ne possède pas déjà la carte destination
+	    if (cm.getDestinationsCards()[i] != null) {
+	        // donne la carte destination et mets à null pour remplacer
+	        this.destinationsList.add(cm.getDestinationsCards()[i]);
+	        cm.getDestinationsCards()[i] = null;
+	        if (!this.destinationsList.isEmpty()) this.canPlay = true;
+	        if (game.getGameFrame().getSound().getclick()) game.getGameFrame().getSound().playSound("INGAME", "carte-dest.wav");
+	        return true;
+	    } else {
+	        if (game.getGameFrame().getSound().getclick()) game.getGameFrame().getSound().playSound("INGAME", "popUp.wav");
+	        JOptionPane.showMessageDialog(new JFrame(), "Vous avez déjà pioché cette carte !", "Instructions", JOptionPane.WARNING_MESSAGE);
+	    }
+	    return false;
 	}
 
+	/**
+	 * Pioche une carte invisible pour le joueur.
+	 */
 	public void piocheCarteInvisible() {
-		CarteManager cm = game.getCarteManager();
+	    CarteManager cm = game.getCarteManager();
 
-		//Si le nombre d'action est égal a 2 alors on pioche une fois et on enleve le nombre d'action -1
-		if(game.getRound().getAction() >1){
-			insertCarte(cm.drawCard());
-			game.getRound().setAction(game.getRound().getAction() - 1);
-
-		}else{
-			//Si on a plus que une action alors on pioche puis on fini le tour
-			insertCarte(cm.drawCard());
-			game.getRound().endRound(game);
-		}
-		if ( game.getGameFrame().getSound().getclick()) game.getGameFrame().getSound().playSound("INGAME" ,"carte-wagon.wav");
+	    // Si le nombre d'actions est égal à 2 alors on pioche une fois et on enlève le nombre d'actions -1
+	    if (game.getRound().getAction() > 1) {
+	        insertCarte(cm.drawCard());
+	        game.getRound().setAction(game.getRound().getAction() - 1);
+	    } else {
+	        // Si on a plus qu'une action alors on pioche puis on finit le tour
+	        insertCarte(cm.drawCard());
+	        game.getRound().endRound(game);
+	    }
+	    if (game.getGameFrame().getSound().getclick()) game.getGameFrame().getSound().playSound("INGAME", "carte-wagon.wav");
 	}
 
+	/**
+	 * Pioche une carte visible de type CarteWagon pour le joueur.
+	 *
+	 * @param carte  La couleur de la carte visible à piocher.
+	 * @return       true si la pioche a réussi, sinon false.
+	 */
 	public boolean piocheCarteVisible(CarteWagon.Couleur carte) {
-
-		//On regarde si le joueur a 2 actions ou non
-		if(game.getRound().getAction() > 1){
-
-			//Si oui alors on regarde si c'est une carte locomotive ou non
-			if(carte == Couleur.LOC){
-				//Si c'est une locomotive on fini le tour du joueur
-				if ( game.getGameFrame().getSound().getclick()) game.getGameFrame().getSound().playSound("INGAME" ,"carte-wagon.wav");
-				insertCarte(carte);
-				game.getRound().endRound(game);
-				return true;
-			}
-			else{
-				//Sinon on enleve une action au joueur
-				if ( game.getGameFrame().getSound().getclick()) game.getGameFrame().getSound().playSound("INGAME" ,"carte-wagon.wav");
-				insertCarte(carte);
-				game.getRound().setAction(game.getRound().getAction() - 1);
-				return true;
-
-			}
-
-		}else{
-			//On verifie que c'est une carte locomotive ou non
-			if(carte == Couleur.LOC){
-				//Si c'est le cas alors on dit qu'on ne peut pas
-				return false;
-			}
-			else{
-				//Sinon on pioche la carte et on passe au joueur suivant
-				if ( game.getGameFrame().getSound().getclick()) game.getGameFrame().getSound().playSound("INGAME" ,"carte-wagon.wav" );
-				insertCarte(carte);
-				game.getRound().endRound(game);
-				return true;
-			}
-		}
+	    // On regarde si le joueur a 2 actions ou non
+	    if (game.getRound().getAction() > 1) {
+	        // Si oui alors on regarde si c'est une carte locomotive ou non
+	        if (carte == Couleur.LOC) {
+	            // Si c'est une locomotive on finit le tour du joueur
+	            if (game.getGameFrame().getSound().getclick()) game.getGameFrame().getSound().playSound("INGAME", "carte-wagon.wav");
+	            insertCarte(carte);
+	            game.getRound().endRound(game);
+	            return true;
+	        } else {
+	            // Sinon on enlève une action au joueur
+	            if (game.getGameFrame().getSound().getclick()) game.getGameFrame().getSound().playSound("INGAME", "carte-wagon.wav");
+	            insertCarte(carte);
+	            game.getRound().setAction(game.getRound().getAction() - 1);
+	            return true;
+	        }
+	    } else {
+	        // On vérifie que c'est une carte locomotive ou non
+	        if (carte == Couleur.LOC) {
+	            // Si c'est le cas alors on dit qu'on ne peut pas
+	            return false;
+	        } else {
+	            // Sinon on pioche la carte et on passe au joueur suivant
+	            if (game.getGameFrame().getSound().getclick()) game.getGameFrame().getSound().playSound("INGAME", "carte-wagon.wav");
+	            insertCarte(carte);
+	            game.getRound().endRound(game);
+	            return true;
+	        }
+	    }
 	}
-
 
 
 	private void insertCarte(Couleur carte)
@@ -228,58 +235,69 @@ public class Player {
 
 	}
 	
-	//Méthode pour retirer la route d'un autre joueur
-	public boolean retirerRouteAutreJoueur(Rail r, Player p) {
-		
-		//Si le joueur n'est pas null
-		if(this != null) {			
-			for(int i = 0; i < this.getPlayerRoutes().size(); i++) {	
-				//Si la Route correspond dans l'inventaire du joueur
-				if(r.getSaRoute() == this.getPlayerRoutes().get(i)) {
-					
-					//Debug : System.out.print("SA PASSE");
-					
-					p.retirerCarteNuke(); //Retire la carte nuke de son inventaire
-					r.getSaRoute().enleverProprio(); //Enlève le proprio de la route et des rails
-					this.getPlayerRoutes().remove(i); //Enlève la route de l'inventaire du joueur
-					
-					//Debug : System.out.println(r.getSaRoute().getProprietaire());
-					
-					return true;
-				}				
-			}			
-		}
-		
-		return false;
-	}
-	
-	//Méthode pour retirer la gare d'un autre joueur
-	public void retirerGareAutrePlayer(Ville ville, Player player) {
-		ville.setIsOccuped(null); //Enlève le proprio de la ville
-		player.retirerCarteNuke(); //Retire la carte nuke de l'inventaire
-		this.nbrGare += 1;  //Rajoute une Gare dispo
-	}
+    /**
+     * Retire une route d'un autre joueur.
+     *
+     * @param r  La route à retirer.
+     * @param p  Le joueur qui effectue l'action de retrait.
+     * @return   true si la route a été retirée avec succès, sinon false.
+     */
+    public boolean retirerRouteAutreJoueur(Rail r, Player p) {
+        // Si le joueur n'est pas null
+        if (this != null) {
+            for (int i = 0; i < this.getPlayerRoutes().size(); i++) {
+                // Si la Route correspond dans l'inventaire du joueur
+                if (r.getSaRoute() == this.getPlayerRoutes().get(i)) {
+                    // Retire la carte nuke de son inventaire
+                    p.retirerCarteNuke();
+                    // Enlève le proprio de la route et des rails
+                    r.getSaRoute().enleverProprio();
+                    // Enlève la route de l'inventaire du joueur
+                    this.getPlayerRoutes().remove(i);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
+    /**
+     * Retire la gare d'un autre joueur.
+     *
+     * @param ville   La ville dont la gare doit être retirée.
+     * @param player  Le joueur qui effectue l'action de retrait.
+     */
+    public void retirerGareAutrePlayer(Ville ville, Player player) {
+        ville.setIsOccuped(null); // Enlève le proprio de la ville
+        player.retirerCarteNuke(); // Retire la carte nuke de l'inventaire
+        this.nbrGare += 1; // Rajoute une gare disponible
+    }
 
-	public boolean checkACarteNuke() {
-		for(int i = 0; i < this.trainList.size(); i++) {
-			if(this.trainList.get(i) == Couleur.NUKE) {
-				return true;
-			}
-		}
-		
-		return false;	
-	}
-	
-	//Méthode pour retirer la carte nuke de l'inventaire d'un joueur
-	private void retirerCarteNuke() {
-		for(int i = 0; i < this.trainList.size(); i++) {
-			if(this.trainList.get(i) == Couleur.NUKE) {
-				this.trainList.remove(i);
-				return;
-			}
-		}
-	}
+    /**
+     * Vérifie si le joueur possède une carte nuke.
+     *
+     * @return  true si le joueur possède une carte nuke, sinon false.
+     */
+    public boolean checkACarteNuke() {
+        for (Couleur couleur : this.trainList) {
+            if (couleur == Couleur.NUKE) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Retire une carte nuke de l'inventaire du joueur.
+     */
+    private void retirerCarteNuke() {
+        for (int i = 0; i < this.trainList.size(); i++) {
+            if (this.trainList.get(i) == Couleur.NUKE) {
+                this.trainList.remove(i);
+                return;
+            }
+        }
+    }
 
 
     public void retirerLesCartes(Couleur color, int carteAEnlever) {
--- src/com/view/CarteDestinationPanel.java
+++ src/com/view/CarteDestinationPanel.java
@@ -1,16 +1,10 @@
 package com.view;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
-
-import javax.swing.*;
-
 import com.model.Game;
 import com.model.Player;
 import com.model.config.carte.CarteDestination;
@@ -18,6 +12,9 @@ import com.model.config.carte.CarteManager;
 import com.model.controller.GameController;
 import com.view.graphics.CardGraphics;
 
+/**
+ * Le panneau représentant la pioche des cartes destination pour le joueur.
+ */
 public class CarteDestinationPanel extends JPanel {
     private GameController gameController;
     private Player player;
@@ -27,8 +24,16 @@ public class CarteDestinationPanel extends JPanel {
     private CarteManager carteDestination;
     private PlayerHandPanel mainDuJoueur;
     private Timer hoverTimer; // Timer pour gérer l'affichage des descriptions
-	private Game game;
+    private Game game;
 
+    /**
+     * Constructeur de CarteDestinationPanel.
+     *
+     * @param width La largeur du panneau.
+     * @param height La hauteur du panneau.
+     * @param playerHandPanel Le panneau de la main du joueur.
+     * @param game Le jeu en cours.
+     */
     public CarteDestinationPanel(int width, int height, PlayerHandPanel playerHandPanel, Game game) {
         setBackground(Color.orange);
         setPreferredSize(new Dimension((int) (width * 0.10), height));
@@ -45,39 +50,40 @@ public class CarteDestinationPanel extends JPanel {
 
         // Initialisation des rectangles pour les cartes
         initRectangles(getWidth(), getHeight());  // Assurez-vous que ceci est appelé avant de définir le bouton
-        
+
         // Création du bouton
         JButton btnPiocherCartes = new JButton("Passer son tour");
 
         // Après l'ajout du bouton au panneau
         btnPiocherCartes.addActionListener(e -> {
             activerPioche();
-            if ( game.getGameFrame().getSound().getclick()) game.getGameFrame().getSound().playSound("INGAME" ,"click.wav" );
+            if (game.getGameFrame().getSound().getclick()) game.getGameFrame().getSound().playSound("INGAME", "click.wav");
             // Rendre le focus au panel du game frame après avoir cliqué sur le bouton
-            game.getGameFrame().requestFocusInWindow() ;
+            game.getGameFrame().requestFocusInWindow();
         });
 
-
-        //taille et position du bouton
+        // Taille et position du bouton
         if (piocheVisibleBounds.length > 0) {
             Rectangle firstCardRect = piocheVisibleBounds[0];
             btnPiocherCartes.setBounds(firstCardRect.x, firstCardRect.y - firstCardRect.height - 10, firstCardRect.width, firstCardRect.height);
         }
-        
-        
 
         // Ajout du bouton au panneau
         this.add(btnPiocherCartes);
     }
 
-    
+    /**
+     * Active la pioche de carte pour le joueur et passe au tour suivant si applicable.
+     */
     private void activerPioche() {
-    	if (player.getFirstTurnOver() && carteDestination.alreadyPickedACard()) {
-	        this.game.getRound().endRound(game);   
-	    }	    	
+        if (player.getFirstTurnOver() && carteDestination.alreadyPickedACard()) {
+            this.game.getRound().endRound(game);
+        }
     }
-    
-    
+
+    /**
+     * Installe l'écouteur de souris pour gérer les clics sur les cartes.
+     */
     private void setupMouseAdapter() {
         addMouseListener(new MouseAdapter() {
             @Override
@@ -105,27 +111,33 @@ public class CarteDestinationPanel extends JPanel {
                     repaint();
                 }
             }
-
         });
     }
-    
-    //Fonction qui fait un timer avant de montrer la carte destination
+
+    /**
+     * Initialise le Timer pour gérer l'affichage des descriptions des cartes.
+     */
     private void initHoverTimer() {
         hoverTimer = new Timer(2000, e -> showCardDescription());
         hoverTimer.setRepeats(false);
     }
 
-    //Fonction qui montre la carte destination
+    /**
+     * Affiche la description de la carte destination survolée.
+     */
     private void showCardDescription() {
         try {
             CarteDestination cD = carteDestination.getDestinationsCards()[this.hoveredCardIndex];
             if (hoveredCardIndex >= 0 && cD != null) { // Vérifie l'index et voit si la carte est nulle
                 this.gameController.descriptionCardDestination(cD, game);
-
             }
-        } catch ( Exception ignored ) { }
+        } catch (Exception ignored) {
+        }
     }
 
+    /**
+     * Installe l'écouteur de souris pour gérer les mouvements de la souris.
+     */
     private void setupMouseMotionListener() {
         addMouseMotionListener(new MouseAdapter() {
             @Override
@@ -147,7 +159,6 @@ public class CarteDestinationPanel extends JPanel {
                     repaint();
                 }
             }
-
         });
     }
 
@@ -160,8 +171,8 @@ public class CarteDestinationPanel extends JPanel {
         for (int i = 0; i < piocheVisibleBounds.length; i++) {
             Rectangle rect = piocheVisibleBounds[i];
             BufferedImage carteVisible = CardGraphics.getCardObjectif();
-            
-            //Si la carte est déjà prise elle deviens grisé (plus prenable)
+
+            // Si la carte est déjà prise elle devient grisée (plus prenable)
             if (carteVisible != null) {
                 if (isCardSelected[i]) {
                     carteVisible = desaturateImage(carteVisible);
@@ -174,12 +185,18 @@ public class CarteDestinationPanel extends JPanel {
             }
         }
     }
-    
+
+    /**
+     * Initialise les rectangles représentant les zones des cartes visibles.
+     *
+     * @param width La largeur du panneau.
+     * @param height La hauteur du panneau.
+     */
     private void initRectangles(int width, int height) {
-        int rectWidth = width; 
+        int rectWidth = width;
         int rectHeight = height / 5;
         int startX = (getWidth() - rectWidth) / 2;
-        int startY = 20; 
+        int startY = 20;
 
         for (int i = 0; i < piocheVisibleBounds.length; i++) {
             startY += rectHeight + 10;
@@ -187,36 +204,45 @@ public class CarteDestinationPanel extends JPanel {
         }
     }
 
-	
-	//Méthode qui rend l'image grise après sélection de la carte
-	public static BufferedImage desaturateImage(BufferedImage image) {
-	    BufferedImage result = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
-	
-	    for (int x = 0; x < image.getWidth(); x++) {
-	        for (int y = 0; y < image.getHeight(); y++) {
-	            Color color = new Color(image.getRGB(x, y));
-	            int r = color.getRed();
-	            int g = color.getGreen();
-	            int b = color.getBlue();
-	            int avg = (r + g + b) / 3; // Calcul de la moyenne des composantes RGB
-	            int gray = (avg << 16) | (avg << 8) | avg; // Conversion en niveau de gris
-	            result.setRGB(x, y, (color.getAlpha() << 24) | gray); // Appliquer le niveau de gris
-	        }
-	    }
-	
-	    return result;
-	}
+    /**
+     * Rend une image en niveau de gris après sélection de la carte.
+     *
+     * @param image L'image originale.
+     * @return L'image désaturée.
+     */
+    public static BufferedImage desaturateImage(BufferedImage image) {
+        BufferedImage result = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                Color color = new Color(image.getRGB(x, y));
+                int r = color.getRed();
+                int g = color.getGreen();
+                int b = color.getBlue();
+                int avg = (r + g + b) / 3; // Calcul de la moyenne des composantes RGB
+                int gray = (avg << 16) | (avg << 8) | avg; // Conversion en niveau de gris
+                result.setRGB(x, y, (color.getAlpha() << 24) | gray); // Appliquer le niveau de gris
+            }
+        }
+
+        return result;
+    }
 
     /* getters et setters */
-	//Remet tout à false pour mettre la bonne couleur (ça évite que les cartes soient grises)
-	public void setAllDefault() {
-		for(int i = 0; i < this.isCardSelected.length; i++)this.isCardSelected[i] = false;
-	}
 
-	public void setPlayer(Player player) {
-	    this.player = player;
-	}
+    /**
+     * Remet tout à false pour réinitialiser l'état des cartes (évite que les cartes soient grises).
+     */
+    public void setAllDefault() {
+        for (int i = 0; i < this.isCardSelected.length; i++) this.isCardSelected[i] = false;
+    }
 
+    /**
+     * Définit le joueur associé à ce panneau.
+     *
+     * @param player Le joueur à associer.
+     */
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
 }
-
-    
